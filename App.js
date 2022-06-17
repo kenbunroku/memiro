82f8beafcb250d00fb83c249c4a860ca34c13057
Shinichiro Kuwahara <kenbunroku524@gmail.com>
2022-06-17 09:13:59 -0500

    Initial commitment

--- App.js
+++ App.js
@@ -1,13 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { useEffect, useState } from 'react'
+import { StyleSheet, Text, View } from 'react-native'
 
 export default function App() {
+  const [data, setData] = useState({})
+  const [name, setName] = useState('')
+
+  useEffect(() => {
+    const url = 'https://km28ifx4dj.execute-api.us-east-1.amazonaws.com/items'
+
+    const fetchData = async () => {
+      const response = await fetch(url)
+      const data = await response.json()
+      setData(data)
+      setName(data.Items[3].first_name)
+    }
+    fetchData()
+  }, [])
+  console.log(data)
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello {name}!</Text>
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +34,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
